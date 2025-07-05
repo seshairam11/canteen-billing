db0bd4708e46fdbf6d870a7e9da63e02016f5780
seshairam11 <4602seshasairam@gmail.com>
2025-07-05 22:34:57 +0530

    add

---
+++ backend/models/hotelBenchModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const hotelBenchSchema = new mongoose.Schema({
+    labelname: { type: String, required: true, trim: true },
+    status: { type: String, required: true },
+    studentName: { type: String, required: false },
+    ChefName: { type: String, required: false },
+    showbutton: { type: Boolean, required: true },
+    ispayment: { type: Boolean, required: true },
+    btn_values: { type: Array, required: true }
+}, {
+    collection: "HotelBench"
+})
+
+
+const HotelBenchModel = mongoose.model('HotelBench', hotelBenchSchema);
+module.exports = HotelBenchModel;
